2fc60e1725550ef1244f0ea146e027d2a981e3da
DeityofDeath <minturao71@gmail.com>
2025-04-06 03:33:47 +0530

    made some more design changes

--- frontend/src/app/student/leave/page.jsx
+++ frontend/src/app/student/leave/page.jsx
@@ -24,7 +24,7 @@ const LeaveApplicationSystem = () => {
   };
 
   return (
-    <div className="min-h-screen w-full bg-gray-50 ">
+    <div className="min-h-screen w-full bg-background">
       <div className="max-w-4xl mx-auto p-4 ">
         {/* Header */}
         <header className="mb-8 text-center">
