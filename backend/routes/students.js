3b283d177ba40d61523b81b0644878e339afd436
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2025-04-06 04:12:46 +0530

    Finished the payment verification route

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,5 +1,7 @@
+const crypto = require("crypto");
 const express = require("express");
 const studentApp = express.Router();
+require("dotenv").config();
 
 const { studentOrAdminLogin, createStudentorAdmin } = require("./util");
 const expressAsyncHandler = require("express-async-handler");
@@ -29,35 +31,65 @@ studentApp.post(
   }),
 );
 
-
 // Attendance -> Mark Entry
-studentApp.post("/attendance/mark", expressAsyncHandler(async (req, res) => {
-  const studentsCollection = req.app.get("studentsCollection");
-  const attendance = req.body;
-  const result = await studentsCollection.updateOne(
-    { email: attendance.email },
-    { $set: { attendance: "true" } }
-  );
-  if (result.modifiedCount === 1) {
-    res.send({ message: "Entrance marked successfully" });
-  } else {
-    res.send({ message: "Failed to mark attendance" });
-  }
-}));
+studentApp.post(
+  "/attendance/mark",
+  expressAsyncHandler(async (req, res) => {
+    const studentsCollection = req.app.get("studentsCollection");
+    const attendance = req.body;
+    const result = await studentsCollection.updateOne(
+      { email: attendance.email },
+      { $set: { attendance: "true" } },
+    );
+    if (result.modifiedCount === 1) {
+      res.send({ message: "Entrance marked successfully" });
+    } else {
+      res.send({ message: "Failed to mark attendance" });
+    }
+  }),
+);
 
 // Attendance -> Mark Exit
-studentApp.post("/attendance/exit", expressAsyncHandler(async (req, res) => {
-  const studentsCollection = req.app.get("studentsCollection");
-  const attendance = req.body;
-  const result = await studentsCollection.updateOne(
-    { email: attendance.email },
-    { $set: { attendance: "false" } }
-  );
-  if (result.modifiedCount === 1) {
-    res.send({ message: "Exit marked successfully" });
-  } else {
-    res.send({ message: "Failed to mark exit" });
-  }
-}));
+studentApp.post(
+  "/attendance/exit",
+  expressAsyncHandler(async (req, res) => {
+    const studentsCollection = req.app.get("studentsCollection");
+    const attendance = req.body;
+    const result = await studentsCollection.updateOne(
+      { email: attendance.email },
+      { $set: { attendance: "false" } },
+    );
+    if (result.modifiedCount === 1) {
+      res.send({ message: "Exit marked successfully" });
+    } else {
+      res.send({ message: "Failed to mark exit" });
+    }
+  }),
+);
+
+studentApp.post(
+  "/verify-payment",
+  expressAsyncHandler(async (req, res) => {
+    const studentsCollection = req.app.get("studentsCollection");
+    const secret = process.env.WEB_HOOK_SECRET;
+    const { email, base_amount } = req.body.payload.payment.entity;
+    const student = await studentsCollection.findOne({ email });
+
+    const shaSum = crypto.createHmac("sha256", secret);
+    shaSum.update(JSON.stringify(req.body));
+    const digest = shaSum.digest("hex");
+
+    if (digest === req.headers["x-razorpay-signature"]) {
+      student.feeDue = (student.feeDue || 5000) - base_amount / 100;
+      const result = await studentsCollection.updateOne(
+        { email: email },
+        { $set: { feeDue: student.feeDue } },
+      );
+      console.log(result);
+    }
+
+    res.send({ status: "ok" });
+  }),
+);
 
 module.exports = studentApp;
